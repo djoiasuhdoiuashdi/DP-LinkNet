b4d36046c69a8554610a31d0516636376a55589c
dom <e12117959@student.tuwien.ac.at>
2025-02-22 10:38:25 +0100

    unify dataset training approach

--- train.py
+++ train.py
@@ -76,7 +76,7 @@ parser.add_argument('--train', type=str, required=True)
 args=parser.parse_args()
 
 SHAPE = (TILE_SIZE, TILE_SIZE)
-train_root = os.path.join("./dataset/", args.train)
+train_root = os.path.join("./dataset/", str(args.train))
 imagelist = list(filter(lambda x: x.find("img") != -1, os.listdir(train_root)))
 trainlist = list(map(lambda x: x[:-8], imagelist))
 
